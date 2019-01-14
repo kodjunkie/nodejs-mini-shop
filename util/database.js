121e5825e61c80f6fb4bafad142d52eb3de97b08
Lawrence Onah <paplow01@gmail.com>
2019-01-14 09:22:48 +0100

    Added configuration to work with mongodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,27 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('v8app', 'root', 'secret', {
-    dialect: 'mysql',
-    host: '127.0.0.1',
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = callback => {
+    MongoClient.connect('mongodb://127.0.0.1:27017', {useNewUrlParser: true})
+        .then(client => {
+            console.log('Connected!');
+            _db = client.db('v8app');
+            callback();
+        })
+        .catch(err => {
+            console.log(err);
+            throw err;
+        });
+};
+
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No Database Found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
