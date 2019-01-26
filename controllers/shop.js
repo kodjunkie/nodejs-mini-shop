e97b3adf0f4aabf4d4ab44ba58a263d18ed73f7c
Lawrence Onah <paplow01@gmail.com>
2019-01-26 17:36:51 +0100

    Added Pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -7,14 +7,31 @@ const Product = require('../models/product');
 const Order = require('../models/order');
 
 const errorHandler = require('../util/error_handler');
+const { ITEMS_PER_PAGE } = require('../util/constants');
 
 exports.getProducts = (req, res, next) => {
-	Product.find()
+	const page = Number.parseInt(req.query.page) || 1;
+	let totalItems;
+
+	Product.countDocuments()
+		.then(numProducts => {
+			totalItems = numProducts;
+			return Product.find()
+				.skip((page - 1) * ITEMS_PER_PAGE)
+				.limit(ITEMS_PER_PAGE);
+		})
 		.then(products => {
 			res.render('shop/product-list', {
 				prods: products,
 				pageTitle: 'All Products',
-				path: '/products'
+				path: '/products',
+				totalProducts: totalItems,
+				currentPage: page,
+				hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+				hasPreviousPage: page > 1,
+				nextPage: page + 1,
+				previousPage: page - 1,
+				lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
 			});
 		})
 		.catch(err => errorHandler(err, next));
@@ -33,12 +50,28 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-	Product.find()
+	const page = Number.parseInt(req.query.page) || 1;
+	let totalItems;
+
+	Product.countDocuments()
+		.then(numProducts => {
+			totalItems = numProducts;
+			return Product.find()
+				.skip((page - 1) * ITEMS_PER_PAGE)
+				.limit(ITEMS_PER_PAGE);
+		})
 		.then(products => {
 			res.render('shop/index', {
 				prods: products,
 				pageTitle: 'Shop',
-				path: '/'
+				path: '/',
+				totalProducts: totalItems,
+				currentPage: page,
+				hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+				hasPreviousPage: page > 1,
+				nextPage: page + 1,
+				previousPage: page - 1,
+				lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
 			});
 		})
 		.catch(err => errorHandler(err, next));
